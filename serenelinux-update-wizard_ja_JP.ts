ae09d67fabce7ec67cf5c3fc92c90cf5a8c83697
kokkiemouse <kokkiemouse@fascode.net>
2021-03-05 22:56:47 +0900

    fixed
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- serenelinux-update-wizard_ja_JP.ts
+++ serenelinux-update-wizard_ja_JP.ts
@@ -1,3 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="serenelinux-update-wizard_ja_JP"></TS>
+<TS version="2.1" language="serenelinux-update-wizard_ja_JP">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="9"/>
+        <source>SereneLinux Update Wizard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="22"/>
+        <source>Xfce mugshot fix</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="32"/>
+        <source>Update Contents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="39"/>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
