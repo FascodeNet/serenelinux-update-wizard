0508a07e4c57b226573ffb6f9dfa7435eb174771
kokkiemouse <kokkiemouse@fascode.net>
2021-03-06 17:33:24 +0900

    added not found
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- serenelinux-update-wizard_ja_JP.ts
+++ serenelinux-update-wizard_ja_JP.ts
@@ -5,34 +5,35 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="build/serenelinux-update-wizard_autogen/include/ui_mainwindow.h" line="67"/>
+        <location filename="build/serenelinux-update-wizard_autogen/include/ui_mainwindow.h" line="86"/>
         <source>Serene Linux Version 33.0.1 Updater</source>
         <translation>Serene Linux Version 33.0.1 Updater</translation>
     </message>
     <message>
-        <location filename="build/serenelinux-update-wizard_autogen/include/ui_mainwindow.h" line="68"/>
         <source>Update List</source>
-        <translation>更新一覧</translation>
+        <translation type="vanished">更新一覧</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="112"/>
-        <location filename="build/serenelinux-update-wizard_autogen/include/ui_mainwindow.h" line="73"/>
+        <location filename="build/serenelinux-update-wizard_autogen/include/ui_mainwindow.h" line="90"/>
         <source>Xfce Mugshot Fixed</source>
         <translation>Xfce Mugshotに関する修正</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="52"/>
-        <location filename="build/serenelinux-update-wizard_autogen/include/ui_mainwindow.h" line="76"/>
+        <location filename="build/serenelinux-update-wizard_autogen/include/ui_mainwindow.h" line="88"/>
         <source>Install</source>
         <translation>インストール</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="35"/>
+        <location filename="build/serenelinux-update-wizard_autogen/include/ui_mainwindow.h" line="87"/>
         <source>These contents will be updated.</source>
         <translation>これらの内容が更新されます</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="75"/>
+        <location filename="build/serenelinux-update-wizard_autogen/include/ui_mainwindow.h" line="89"/>
         <source>Cancel</source>
         <translation>キャンセル</translation>
     </message>
